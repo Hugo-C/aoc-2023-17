b6773e7cc84c12e7bf983873ebe530b4e38b4c5a
Hugo-C <24675917+Hugo-C@users.noreply.github.com>
2024-07-20 01:44:59 +0200

    initial commit

---
+++ dijkstra.py
@@ -0,0 +1,96 @@
+from typing import Self
+
+
+class Node:
+    """Node of a non oriented graph"""
+
+    def __init__(self, name: str):
+        self.name = name
+        self.links = dict()
+
+    def add_link(self, node: Self, weight: int, reverse=False):
+        self.links[node] = weight
+        if not reverse:
+            node.add_link(self, weight, reverse=True)
+
+    def __repr__(self):
+        return f"Node {self.name}-#{len(self.links)}{[(node.name, weight) for (node, weight) in self.links.items()]}"
+
+
+def get_closest_node(distances, nodes_done) -> Node:
+    min_weight = None
+    closest_node = None
+    for (node_1, node_2), weight in distances.items():
+        # The 2 nodes cannot be both outside of nodes_done
+        if node_1 not in nodes_done and (min_weight is None or weight < min_weight):
+            min_weight = weight
+            closest_node = node_1
+        if node_2 not in nodes_done and (min_weight is None or weight < min_weight):
+            min_weight = weight
+            closest_node = node_2
+    return closest_node
+
+
+def build_result(distances, start_node: Node, end_node: Node) -> list[Node]:
+    current_node = end_node
+    path = [current_node]
+    while current_node != start_node:
+        min_weight = None
+        closest_node = None
+        for (node_1, node_2), weight in distances.items():
+            if node_2 == current_node and node_1 not in path and (min_weight is None or weight < min_weight):
+                min_weight = weight
+                closest_node = node_1
+        current_node = closest_node
+        path.append(current_node)
+    return list(reversed(path))
+
+
+def dijkstra(start: Node, end: Node) -> list[Node]:
+    distances = {}  # key: Node 1 to Node 2 (tuple), value: weight (int)
+    nodes_done = set()
+    current_node = start
+    while end not in nodes_done:
+        for linked_node, weight in current_node.links.items():
+            existing_weight = distances.get((current_node, linked_node))
+            if not existing_weight or existing_weight > weight:
+                distances[(current_node, linked_node)] = weight
+        nodes_done.add(current_node)
+        current_node = get_closest_node(distances, nodes_done)
+
+    return build_result(distances, start, end)
+
+
+if __name__ == '__main__':
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+    g = Node("g")
+    h = Node("h")
+    i = Node("i")
+    j = Node("j")
+
+    a.add_link(b, 85)
+    b.add_link(f, 80)
+    f.add_link(i, 250)
+    i.add_link(j, 84)
+    a.add_link(c, 217)
+    c.add_link(g, 186)
+    c.add_link(h, 103)
+    h.add_link(d, 183)
+    h.add_link(j, 167)
+    a.add_link(e, 173)
+    e.add_link(j, 502)
+
+    result = dijkstra(a, j)
+    print(f"path={[node.name for node in result]}")
+    length = 0
+    for i in range(1, len(result)):
+        start = result[i-1]
+        end = result[i]
+        length += start.links[end]
+
+    print(f"{length=}")
---
+++ test_dijstra.py
@@ -0,0 +1,44 @@
+from hypothesis import given
+from hypothesis.strategies import composite, integers, lists
+
+from dijkstra import dijkstra, Node
+
+
+@composite
+def bamboo_graph(draw, elements=integers(min_value=0)) -> Node:
+    xs = draw(lists(elements, min_size=2))
+    prev_node: Node = None
+    for i, weigth in enumerate(xs):
+        n = Node(name=str(i))
+        if prev_node:
+            prev_node.add_link(n, weigth)
+        prev_node = n
+    return n
+
+
+@given(bamboo_graph())
+def test_dijkstra_result_begin_and_end_with_given_parameters(start_node: Node):
+    next_node = list(start_node.links.keys())[0]
+
+    node_list = dijkstra(start_node, next_node)
+
+    assert node_list[0] == start_node
+    assert node_list[1] == next_node
+
+
+@given(bamboo_graph())
+def test_dijkstra_result_for_a_bamboo_is_simply_the_list_of_nodes(start_node: Node):
+    nodes = [start_node]
+    node = start_node
+    node_found = True
+    while node_found:
+        node_found = False
+        for n in node.links.keys():
+            if n not in nodes:
+                nodes.append(n)
+                node = n
+                node_found = True
+
+    node_list = dijkstra(nodes[0], nodes[-1])
+
+    assert node_list == nodes
